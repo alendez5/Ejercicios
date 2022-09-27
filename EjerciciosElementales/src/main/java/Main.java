9382c799840229ec4b9fca7278f77899c75f3506
alendez5 <alejandro.menendezcanache@tiernoparla.es>
2022-09-27 20:03:09 +0200

    ejercicio1 y 2

--- EjerciciosElementales/src/main/java/Main.java
+++ EjerciciosElementales/src/main/java/Main.java
@@ -10,8 +10,41 @@
  */
 public class Main {
     public static void main(String args[]){
+        
+            System.out.println("Ejercicio 1: ");
+            
+            System.out.println();
+            ejercicio1Func();
+            System.out.println();
+            
+            System.out.println("Ejercicio 2: ");
+            
+            System.out.println();
+            ejercicio2Func();
+            System.out.println();
+            
+            
+    }
+    
+    private static void ejercicio1Func(){
         for(int i = 0; i <= 10; i+=2){
             System.out.println(i);
         }
     }
+    
+    private static void ejercicio2Func(){
+        
+        int dividendo = 10;
+        int divisor = 2;
+        int q = 0;
+        
+        while (dividendo >= divisor){
+            dividendo -= divisor;
+            q += 1;
+        } 
+        
+        System.out.println(q);
+    }
+    
+    
 }
