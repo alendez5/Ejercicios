ed165f480caa9ed39d2117acc7fbe801b03cd3ce
IvanRdaw <ivanr.daw@gmail.com>
2022-09-29 16:36:49 +0200

    prueba commit desde nuevo portatil

--- EjerciciosElementales/src/main/java/Main.java
+++ EjerciciosElementales/src/main/java/Main.java
@@ -1,50 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author vespertino
- */
 public class Main {
-    public static void main(String args[]){
-        
-            System.out.println("Ejercicio 1: ");
-            
-            System.out.println();
-            ejercicio1Func();
-            System.out.println();
-            
-            System.out.println("Ejercicio 2: ");
-            
-            System.out.println();
-            ejercicio2Func();
-            System.out.println();
+
+    public static void main(String args[]) {
+
+        for (int i = 1; i < 11;i++){
             
             
-    }
-    
-    private static void ejercicio1Func(){
-        for(int i = 0; i <= 10; i+=2){
-            System.out.println(i);
+            if(i % 2 == 0){
+                 System.out.println(i);
+            }
+            
+            
         }
+            
+        
+        
+        int n = 0;
     }
-    
-    private static void ejercicio2Func(){
-        
-        int dividendo = 10;
-        int divisor = 2;
-        int q = 0;
-        
-        while (dividendo >= divisor){
-            dividendo -= divisor;
-            q += 1;
-        } 
-        
-        System.out.println(q);
-    }
-    
-    
+
+ 
 }
