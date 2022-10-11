6b3c2d176c693f2a97f0412bd7ad170fc45e16ed
IvanRdaw <ivanr.daw@gmail.com>
2022-10-11 17:29:31 +0200

    primer push de rama1

--- EjerciciosElementales/src/main/java/Main.java
+++ EjerciciosElementales/src/main/java/Main.java
@@ -1,22 +1,26 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
 
+ 
     public static void main(String args[]) {
-
-        for (int i = 1; i < 11;i++){
-            
-            
-            if(i % 2 == 0){
-                 System.out.println(i);
-            }
-            
-            
+      
+        String palabra = "abcdef";
+        char c =' ';
+        String par;
+        
+        if(palabra.length() %2 == 0){
+            par = palabra.charAt(palabra.length()/2-1) + "" + palabra.charAt(palabra.length()/2);
+            System.out.println(par);
         }
-            
-        
-        
-        int n = 0;
+     
+      
     }
 
  
+
 }
+
+
